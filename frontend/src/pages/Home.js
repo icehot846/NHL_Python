3f4a21fdd5fada5e74867770163c255329a21538
Zach <zdberghoff@gmail.com>
2025-03-26 19:09:41 -0500

    Fixed nhl logo

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import './Home.css';
+import NHLStatsIcon from "../assets/logos/NHL_Stats_Icon.png";
+
 
 function Home() {
     const navigate = useNavigate();
@@ -9,7 +11,7 @@ function Home() {
         <div className="container">
             <div className="hero">
                 <div className="hero-content">
-                    <img src="./logos/NHL_Stats_Icon.png" alt="NHL Logo" className="hero-logo" />
+                    <img src={NHLStatsIcon} alt="NHL Logo" className="hero-logo" />
                     <h1>Welcome to NHL Stats</h1>
                     <p>Explore real-time stats, team details, and player performances across the NHL.</p>
                     <button onClick={() => navigate("/teams")}>Explore Teams</button>
