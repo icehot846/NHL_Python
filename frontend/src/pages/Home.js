a5720ab5c5be353136bddb884512c11dc0d1b18a
Zach <zdberghoff@gmail.com>
2025-03-26 19:01:57 -0500

    Changes to frontend home page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -3,15 +3,19 @@ import { useNavigate } from "react-router-dom";
 import './Home.css';
 
 function Home() {
-    const navigate = useNavigate(); // ✅ useNavigate inside a functional component
+    const navigate = useNavigate();
 
     return (
-        <div className="hero">
-            <h1>Welcome to NHL Stats</h1>
-            <p>Explore real-time stats across all NHL teams and players.</p> 
-            <button onClick={() => navigate("/teams")}>Explore Teams</button>
+        <div className="container">
+            <div className="hero">
+                <div className="hero-content">
+                    <img src="./logos/NHL_Stats_Icon.png" alt="NHL Logo" className="hero-logo" />
+                    <h1>Welcome to NHL Stats</h1>
+                    <p>Explore real-time stats, team details, and player performances across the NHL.</p>
+                    <button onClick={() => navigate("/teams")}>Explore Teams</button>
+                </div>
+            </div>
         </div>
-      
     );
 }
 
